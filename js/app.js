8a4fe907b757e41fcd4a1c883cd1e59d85ee2045
Race Freeze <Mathofreeze@gmail.com>
2024-06-12 18:11:27 -0600

    Add code for sidebar.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let cart = [];
+
+let sideBarContainer = document.querySelector('#sideBarContainer');
+
+function clearStorageAndCart() {
+    cart = [];
+    let currentStorage = JSON.parse(localStorage.getItem('CART'));
+    if(currentStorage) {
+        cart = currentStorage;
+    }
+}
--- js/shop.js
+++ js/shop.js
@@ -3,7 +3,8 @@ let cardContainer = document.querySelector("#cardContainer");
 function addProducts() {
     products.forEach(
         individualCard => cardContainer.innerHTML += 
-        `<div class="card" id="cardNumber${individualCard.id}">
+        `
+        <div class="card" id="cardNumber${individualCard.id}">
             <img src="${imagePath}${individualCard.image}" alt="${individualCard.description}">
             <div class="cardText">
                 <h4>${individualCard.name}</h4>
@@ -11,8 +12,40 @@ function addProducts() {
                 <p>&dollar; ${individualCard.price}</p>
                 <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
             </div>
-        </div>`
+        </div>
+        `
     );
 }
 addProducts();
 
+function saveToLocalStorage() {
+    let cartButton = document.getElementsByClassName("cartButton");
+    for (let i = 0; i < cartButton.length; i++) {
+        cartButton[i].addEventListener('click', function() {
+            let selectedProduct = products.find((product) => product.id == cartButton[i].id);
+            cart.push(selectedProduct);
+            localStorage.setItem('CART', JSON.stringify(cart));
+            displayCart();
+        }, false);
+    }
+}
+saveToLocalStorage();
+
+function displayCart() {
+    clearStorageAndCart();
+    sideBarContainer.innerHTML = "";
+    cart.forEach(
+        cartItem => sideBarContainer.innerHTML +=
+        `
+        <div class="tableRow">
+            <div class="tableCell">${cartItem.name}</div>
+            <div class="tableCell">&dollar; ${cartItem.price}</div>
+        </div>
+        <div class="tableRow">
+            <div class="tableCell borderBottom"></div>
+            <a class="tableCell borderBottom removeLink" href="#" id="${cartItem.id}">Remove</a>
+        </div>
+        `
+    );
+}
+displayCart();
